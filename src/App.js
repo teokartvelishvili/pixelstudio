fc7dbd6532af69e66eafcdfa3801875afae68fab
teokartvelishvili <qartvelishviliteona@gmail.com>
2024-12-29 00:28:35 -0800

    initial

---
+++ src/App.js
@@ -0,0 +1,69 @@
+
+import React, { useState, useEffect } from 'react';
+import './App.css';
+import Layout from './Layouts/Layout.js'
+import Home from './Pages/Home/Home.jsx';
+import About from './Pages/About/About.jsx';
+import Contact from './Pages/Contact/Contact.jsx';
+import { ThemeContext } from './Hooks/ThemeContext.js'
+import { LanguageContext } from "../src/Hooks/LanguageContext";
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+
+function App() {
+  const [theme, setTheme] = useState('light');
+  const [language, setLanguage] = useState("ge");
+
+  useEffect(() => {
+    const appEl = document.getElementById('app');
+    if (appEl) {
+      appEl.className = theme;
+    }
+  }, [theme]);
+  useEffect(() => {
+    document.body.className = language;
+  }, [language]);
+
+  const toggleTheme = () => {
+    setTheme((prevTheme) => (prevTheme === 'dark' ? 'light' : 'dark'));
+  };
+
+  return (
+    <Router>
+      <div className="App" id="app">
+        <ThemeContext.Provider value={{ theme, toggleTheme }}>
+        <LanguageContext.Provider value={{ language, setLanguage }}>
+          <Routes>
+            <Route
+              path="/"
+              element={
+                <Layout>
+                  <Home />
+                </Layout>
+              }
+            />
+            <Route
+              path="/about"
+              element={
+                <Layout>
+                  <About/>
+                </Layout>
+              }
+            />
+        
+            <Route
+              path="/contact"
+              element={
+                <Layout>
+                  <Contact />
+                </Layout>
+              }
+            />
+          </Routes>
+          </LanguageContext.Provider>
+        </ThemeContext.Provider>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
---
+++ src/Hooks/Languages.js
@@ -0,0 +1,14 @@
+export const TEXTS = {
+    ge: {
+      home: "მთავარი",
+      about: "ჩვენს შესახებ",
+      contact: "კონტაქტი",
+      
+    },
+  
+    en: {
+      home: "HOME",
+      about: "About",
+      contact: "Contact",
+    },
+  };
