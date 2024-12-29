be66f8d078bcc7b53ca3c3bb4c27f85b237f2937
anano303 <124179773+anano303@users.noreply.github.com>
2024-12-30 02:33:30 +0400

    .

--- src/Data/CardsData.js
+++ src/Data/CardsData.js
@@ -1,43 +1,65 @@
-import baby from '../Assets/baby.jpg'
+import baby from "../Assets/baby.jpg";
 
 const cardsData = [
   {
-    id: '1',
+    id: "1",
     image: baby,
-    category: 'ბავშვთა სესიები',
-    photos: ['https://scontent.ftbs5-2.fna.fbcdn.net/v/t39.30808-6/470215835_1028234319345238_448142179135476230_n.jpg?_nc_cat=111&ccb=1-7&_nc_sid=127cfc&_nc_ohc=VJ1bJgpfEZoQ7kNvgE5u8xi&_nc_zt=23&_nc_ht=scontent.ftbs5-2.fna&_nc_gid=AYCmOCaqovfHtvcGlJIgi3Y&oh=00_AYCs5Fd9zRvMEsoVJcZZyUvKtl1ZGxtI7pskIlx8TUjFUQ&oe=67772238', 
-      'https://scontent.ftbs5-2.fna.fbcdn.net/v/t39.30808-6/414077713_764922602343079_4223024216621745984_n.jpg?_nc_cat=110&ccb=1-7&_nc_sid=127cfc&_nc_ohc=TaKni9ULBdcQ7kNvgE6HbYd&_nc_zt=23&_nc_ht=scontent.ftbs5-2.fna&_nc_gid=AZcn3oO5-tzBIfbnI9pg4Nv&oh=00_AYCiPTYRf2fwgsmRvuGnkZWcuoDsvs7BmbAyDX-GM8Yu2Q&oe=67771196'],
-  },
-  {
-    id: '2',
-    image: 'https://scontent.ftbs5-3.fna.fbcdn.net/v/t1.6435-9/29260999_945961628895605_4591102546575097856_n.jpg?_nc_cat=109&ccb=1-7&_nc_sid=a5f93a&_nc_ohc=274uPBIqCs4Q7kNvgHevnWT&_nc_zt=23&_nc_ht=scontent.ftbs5-3.fna&_nc_gid=AOYd7aNofbJgNf9siCtoO8M&oh=00_AYDR4kTYudPSEO6QJzYTk_Y8rCfsDY5oBSXOm5RSj1EMIw&oe=67988C2A',
-    category: 'ბანკეტები, ბოლოზარები',
+    category: "ბავშვთა სესიები",
     photos: [
-      'https://scontent.ftbs5-2.fna.fbcdn.net/v/t39.30808-6/424546684_841967804638558_3239330826980874288_n.jpg?stp=cp6_dst-jpg_tt6&_nc_cat=111&ccb=1-7&_nc_sid=127cfc&_nc_ohc=TtDQ9hSBurwQ7kNvgEbaXrm&_nc_zt=23&_nc_ht=scontent.ftbs5-2.fna&_nc_gid=AlNDS2nwRpFLSwNhMo5GhEa&oh=00_AYBQC2Pva0xtN1FvqOPhZmHRj-BHYQ__a2lSvv0ZeIQgEw&oe=67771D77',
-      'https://scontent.ftbs5-2.fna.fbcdn.net/v/t39.30808-6/440749908_841967874638551_8349205088154347824_n.jpg?stp=cp6_dst-jpg_tt6&_nc_cat=104&ccb=1-7&_nc_sid=127cfc&_nc_ohc=Cfy3nSbBplcQ7kNvgFQNH8d&_nc_zt=23&_nc_ht=scontent.ftbs5-2.fna&_nc_gid=AncnSBQae4lEZjDKMemMYdN&oh=00_AYCBGamub--e5YnotqEhVHxDW0wuPzh4suCIeEgMhOvvWQ&oe=67770A32',
-       'https://scontent.ftbs5-2.fna.fbcdn.net/v/t39.30808-6/440578200_841967707971901_6045347042112531352_n.jpg?stp=cp6_dst-jpg_tt6&_nc_cat=111&ccb=1-7&_nc_sid=127cfc&_nc_ohc=qzoKIgHI4YoQ7kNvgEhiaU6&_nc_zt=23&_nc_ht=scontent.ftbs5-2.fna&_nc_gid=A8e-kR0AwA_Eq4nqlgPHP2X&oh=00_AYCP7OoqyY205s-Kv56shjPqqdV3CJlrZbL9BT-Y3CxIgw&oe=677714CE',
-       'https://scontent.ftbs5-2.fna.fbcdn.net/v/t39.30808-6/440578200_841967707971901_6045347042112531352_n.jpg?stp=cp6_dst-jpg_tt6&_nc_cat=111&ccb=1-7&_nc_sid=127cfc&_nc_ohc=qzoKIgHI4YoQ7kNvgEhiaU6&_nc_zt=23&_nc_ht=scontent.ftbs5-2.fna&_nc_gid=A8e-kR0AwA_Eq4nqlgPHP2X&oh=00_AYCP7OoqyY205s-Kv56shjPqqdV3CJlrZbL9BT-Y3CxIgw&oe=677714CE'],
+      "https://res.cloudinary.com/dbc02mn7z/image/upload/v1735507673/_MG_8458_jfaoh2.jpg",
+      "https://res.cloudinary.com/dbc02mn7z/image/upload/v1735507671/_MG_8433_qrgqi9.jpg",
+      "https://res.cloudinary.com/dbc02mn7z/image/upload/v1735507663/IMG_4873_hkjas8.jpg",
+      "https://res.cloudinary.com/dbc02mn7z/image/upload/v1735507663/IMG_4868_tmqeus.jpg",
+      "https://res.cloudinary.com/dbc02mn7z/image/upload/v1735507663/IMG_48522_f0jjab.jpg",
+      "https://res.cloudinary.com/dbc02mn7z/image/upload/v1735507659/IMG_4867_e1yvof.jpg",
+      "https://res.cloudinary.com/dbc02mn7z/image/upload/v1735507657/_MG_5635_jsujwk.jpg",
+      "https://res.cloudinary.com/dbc02mn7z/image/upload/v1735507655/_MG_3497_-_Copy_fok6z4.jpg",
+      "https://res.cloudinary.com/dbc02mn7z/image/upload/v1735507655/IMG_4852_lg5asc.jpg",
+      "https://res.cloudinary.com/dbc02mn7z/image/upload/v1735507650/_MG_0060_m9jusp.jpg",
+      "https://res.cloudinary.com/dbc02mn7z/image/upload/v1735507641/FB_IMG_1652600979439_sawfuc.jpg",
+    ],
   },
   {
-    id: '3',
-    image: 'https://scontent.ftbs5-3.fna.fbcdn.net/v/t39.30808-6/470124565_1026134786221858_3814634485070654587_n.jpg?stp=cp6_dst-jpg_tt6&_nc_cat=102&ccb=1-7&_nc_sid=127cfc&_nc_ohc=UlffNZHG3QEQ7kNvgG-q2Ik&_nc_zt=23&_nc_ht=scontent.ftbs5-3.fna&_nc_gid=ANawYK0prIF6S7qhZxPOQVF&oh=00_AYDg2gBYcz3g8zTXtlHiJuCPE_UPj9R4aEArycGQtoT_gw&oe=677717A6',
-    category: 'გარე საიმიჯო',
-    photos: ['https://scontent.ftbs5-3.fna.fbcdn.net/v/t39.30808-6/469977751_1025802742921729_4558848689096658767_n.jpg?stp=cp6_dst-jpg_tt6&_nc_cat=106&ccb=1-7&_nc_sid=127cfc&_nc_ohc=RGdWm19z0MIQ7kNvgG19lry&_nc_zt=23&_nc_ht=scontent.ftbs5-3.fna&_nc_gid=A6J1IKzFUQc_0getMRE6nxf&oh=00_AYAJ6dXrRPqoT27BO_y8cT_rbeaE2CJ7BsCpJJEbdb5hAA&oe=677732AB',
-       'https://scontent.ftbs5-4.fna.fbcdn.net/v/t39.30808-6/469891022_1025802706255066_6199254949333718878_n.jpg?stp=cp6_dst-jpg_tt6&_nc_cat=101&ccb=1-7&_nc_sid=127cfc&_nc_ohc=_VXo3KT9NBQQ7kNvgHIFRke&_nc_zt=23&_nc_ht=scontent.ftbs5-4.fna&_nc_gid=AaHXh36CMzgoVKowuT3kxBj&oh=00_AYCT48r49YIntKn0ewrwOpMqMdcdLyd8_6enENIQ97m7CA&oe=67772405'],
+    id: "2",
+    image:
+      "https://scontent.ftbs5-3.fna.fbcdn.net/v/t1.6435-9/29260999_945961628895605_4591102546575097856_n.jpg?_nc_cat=109&ccb=1-7&_nc_sid=a5f93a&_nc_ohc=274uPBIqCs4Q7kNvgHevnWT&_nc_zt=23&_nc_ht=scontent.ftbs5-3.fna&_nc_gid=AOYd7aNofbJgNf9siCtoO8M&oh=00_AYDR4kTYudPSEO6QJzYTk_Y8rCfsDY5oBSXOm5RSj1EMIw&oe=67988C2A",
+    category: "ბანკეტები, ბოლოზარები",
+    photos: [
+      "https://scontent.ftbs5-2.fna.fbcdn.net/v/t39.30808-6/424546684_841967804638558_3239330826980874288_n.jpg?stp=cp6_dst-jpg_tt6&_nc_cat=111&ccb=1-7&_nc_sid=127cfc&_nc_ohc=TtDQ9hSBurwQ7kNvgEbaXrm&_nc_zt=23&_nc_ht=scontent.ftbs5-2.fna&_nc_gid=AlNDS2nwRpFLSwNhMo5GhEa&oh=00_AYBQC2Pva0xtN1FvqOPhZmHRj-BHYQ__a2lSvv0ZeIQgEw&oe=67771D77",
+      "https://scontent.ftbs5-2.fna.fbcdn.net/v/t39.30808-6/440749908_841967874638551_8349205088154347824_n.jpg?stp=cp6_dst-jpg_tt6&_nc_cat=104&ccb=1-7&_nc_sid=127cfc&_nc_ohc=Cfy3nSbBplcQ7kNvgFQNH8d&_nc_zt=23&_nc_ht=scontent.ftbs5-2.fna&_nc_gid=AncnSBQae4lEZjDKMemMYdN&oh=00_AYCBGamub--e5YnotqEhVHxDW0wuPzh4suCIeEgMhOvvWQ&oe=67770A32",
+      "https://scontent.ftbs5-2.fna.fbcdn.net/v/t39.30808-6/440578200_841967707971901_6045347042112531352_n.jpg?stp=cp6_dst-jpg_tt6&_nc_cat=111&ccb=1-7&_nc_sid=127cfc&_nc_ohc=qzoKIgHI4YoQ7kNvgEhiaU6&_nc_zt=23&_nc_ht=scontent.ftbs5-2.fna&_nc_gid=A8e-kR0AwA_Eq4nqlgPHP2X&oh=00_AYCP7OoqyY205s-Kv56shjPqqdV3CJlrZbL9BT-Y3CxIgw&oe=677714CE",
+      "https://scontent.ftbs5-2.fna.fbcdn.net/v/t39.30808-6/440578200_841967707971901_6045347042112531352_n.jpg?stp=cp6_dst-jpg_tt6&_nc_cat=111&ccb=1-7&_nc_sid=127cfc&_nc_ohc=qzoKIgHI4YoQ7kNvgEhiaU6&_nc_zt=23&_nc_ht=scontent.ftbs5-2.fna&_nc_gid=A8e-kR0AwA_Eq4nqlgPHP2X&oh=00_AYCP7OoqyY205s-Kv56shjPqqdV3CJlrZbL9BT-Y3CxIgw&oe=677714CE",
+    ],
   },
   {
-    id: '4',
-    image: 'https://scontent.ftbs5-4.fna.fbcdn.net/v/t39.30808-6/470222830_1026134759555194_1913859271021358416_n.jpg?stp=cp6_dst-jpg_tt6&_nc_cat=100&ccb=1-7&_nc_sid=127cfc&_nc_ohc=d81lOeDYL2YQ7kNvgHLYV7_&_nc_zt=23&_nc_ht=scontent.ftbs5-4.fna&_nc_gid=A04fR9J5osMiufyIvDTNe8P&oh=00_AYBqUdKuwaJcuHlvRXEhNJJYmnpYqpqA7NokoZN7KTFLtQ&oe=6777040B',
-    category: 'კონცერტები, სცენა',
-    photos: ['https://scontent.ftbs5-4.fna.fbcdn.net/v/t39.30808-6/470227207_1028234049345265_4427732001773645205_n.jpg?_nc_cat=100&ccb=1-7&_nc_sid=127cfc&_nc_ohc=nNrEhMLSAZ8Q7kNvgHO6bZ_&_nc_zt=23&_nc_ht=scontent.ftbs5-4.fna&_nc_gid=AvBVGK-7iwjL0Ec1qnYxFx-&oh=00_AYBvVbVRP88r8kc3ZuI0JRm6JXBcKdrUphNWxi_XijKlgg&oe=67771BD8',
-       'https://scontent.ftbs5-4.fna.fbcdn.net/v/t39.30808-6/470226776_1028234069345263_8798733882286682774_n.jpg?_nc_cat=101&ccb=1-7&_nc_sid=127cfc&_nc_ohc=p7l8fGlb2vsQ7kNvgEwRNiA&_nc_zt=23&_nc_ht=scontent.ftbs5-4.fna&_nc_gid=Ao5D_BE9tRA1tUTRtgU2y7K&oh=00_AYBY_8S2B_Ct07Rj34HsAEp6x4gGKdfNHmU-_ptWdCYgJw&oe=67771AF7'],
+    id: "3",
+    image:
+      "https://scontent.ftbs5-3.fna.fbcdn.net/v/t39.30808-6/470124565_1026134786221858_3814634485070654587_n.jpg?stp=cp6_dst-jpg_tt6&_nc_cat=102&ccb=1-7&_nc_sid=127cfc&_nc_ohc=UlffNZHG3QEQ7kNvgG-q2Ik&_nc_zt=23&_nc_ht=scontent.ftbs5-3.fna&_nc_gid=ANawYK0prIF6S7qhZxPOQVF&oh=00_AYDg2gBYcz3g8zTXtlHiJuCPE_UPj9R4aEArycGQtoT_gw&oe=677717A6",
+    category: "გარე საიმიჯო",
+    photos: [
+      "https://scontent.ftbs5-3.fna.fbcdn.net/v/t39.30808-6/469977751_1025802742921729_4558848689096658767_n.jpg?stp=cp6_dst-jpg_tt6&_nc_cat=106&ccb=1-7&_nc_sid=127cfc&_nc_ohc=RGdWm19z0MIQ7kNvgG19lry&_nc_zt=23&_nc_ht=scontent.ftbs5-3.fna&_nc_gid=A6J1IKzFUQc_0getMRE6nxf&oh=00_AYAJ6dXrRPqoT27BO_y8cT_rbeaE2CJ7BsCpJJEbdb5hAA&oe=677732AB",
+      "https://scontent.ftbs5-4.fna.fbcdn.net/v/t39.30808-6/469891022_1025802706255066_6199254949333718878_n.jpg?stp=cp6_dst-jpg_tt6&_nc_cat=101&ccb=1-7&_nc_sid=127cfc&_nc_ohc=_VXo3KT9NBQQ7kNvgHIFRke&_nc_zt=23&_nc_ht=scontent.ftbs5-4.fna&_nc_gid=AaHXh36CMzgoVKowuT3kxBj&oh=00_AYCT48r49YIntKn0ewrwOpMqMdcdLyd8_6enENIQ97m7CA&oe=67772405",
+    ],
   },
   {
-    id: '5',
-    image: 'https://scontent.ftbs5-3.fna.fbcdn.net/v/t39.30808-6/452038588_892421286259876_4120679565600476249_n.jpg?stp=cp6_dst-jpg_tt6&_nc_cat=107&ccb=1-7&_nc_sid=127cfc&_nc_ohc=aa08-ozdvrkQ7kNvgGr_gbP&_nc_zt=23&_nc_ht=scontent.ftbs5-3.fna&_nc_gid=AQ-CwYmHo_lAPcRq_qVu6We&oh=00_AYDDrgY8JC5CdSrdtts3u8fZbiyK3mFEzb1ebmnXi2ZKqA&oe=67771DC8',
-    category: 'გარე მარკეტინგული',
-    photos: ['https://scontent.ftbs5-3.fna.fbcdn.net/v/t39.30808-6/451588293_892421192926552_8467713785957959122_n.jpg?stp=cp6_dst-jpg_tt6&_nc_cat=106&ccb=1-7&_nc_sid=127cfc&_nc_ohc=k_d4sk8FM24Q7kNvgFAW3YR&_nc_zt=23&_nc_ht=scontent.ftbs5-3.fna&_nc_gid=AqnSmJjj5_kKoon-uE0iL9k&oh=00_AYAyS56rpB1flzkL7Bs-_BahaHKdf9Hf07ec3pMJWpT1Iw&oe=6777275A', 
-     'https://scontent.ftbs5-3.fna.fbcdn.net/v/t39.30808-6/451872777_892421179593220_1921483017377968548_n.jpg?stp=cp6_dst-jpg_tt6&_nc_cat=109&ccb=1-7&_nc_sid=127cfc&_nc_ohc=cTRDWWc81GgQ7kNvgFLFEfY&_nc_zt=23&_nc_ht=scontent.ftbs5-3.fna&_nc_gid=AbEclkdcJLlmsoo295a2XFc&oh=00_AYD6vs35eHHbq9NawIzd6mogDsu_vUBVbQryZPfFVvfE5Q&oe=67770660'],
+    id: "4",
+    image:
+      "https://scontent.ftbs5-4.fna.fbcdn.net/v/t39.30808-6/470222830_1026134759555194_1913859271021358416_n.jpg?stp=cp6_dst-jpg_tt6&_nc_cat=100&ccb=1-7&_nc_sid=127cfc&_nc_ohc=d81lOeDYL2YQ7kNvgHLYV7_&_nc_zt=23&_nc_ht=scontent.ftbs5-4.fna&_nc_gid=A04fR9J5osMiufyIvDTNe8P&oh=00_AYBqUdKuwaJcuHlvRXEhNJJYmnpYqpqA7NokoZN7KTFLtQ&oe=6777040B",
+    category: "კონცერტები, სცენა",
+    photos: [
+      "https://scontent.ftbs5-4.fna.fbcdn.net/v/t39.30808-6/470227207_1028234049345265_4427732001773645205_n.jpg?_nc_cat=100&ccb=1-7&_nc_sid=127cfc&_nc_ohc=nNrEhMLSAZ8Q7kNvgHO6bZ_&_nc_zt=23&_nc_ht=scontent.ftbs5-4.fna&_nc_gid=AvBVGK-7iwjL0Ec1qnYxFx-&oh=00_AYBvVbVRP88r8kc3ZuI0JRm6JXBcKdrUphNWxi_XijKlgg&oe=67771BD8",
+      "https://scontent.ftbs5-4.fna.fbcdn.net/v/t39.30808-6/470226776_1028234069345263_8798733882286682774_n.jpg?_nc_cat=101&ccb=1-7&_nc_sid=127cfc&_nc_ohc=p7l8fGlb2vsQ7kNvgEwRNiA&_nc_zt=23&_nc_ht=scontent.ftbs5-4.fna&_nc_gid=Ao5D_BE9tRA1tUTRtgU2y7K&oh=00_AYBY_8S2B_Ct07Rj34HsAEp6x4gGKdfNHmU-_ptWdCYgJw&oe=67771AF7",
+    ],
+  },
+  {
+    id: "5",
+    image:
+      "https://scontent.ftbs5-3.fna.fbcdn.net/v/t39.30808-6/452038588_892421286259876_4120679565600476249_n.jpg?stp=cp6_dst-jpg_tt6&_nc_cat=107&ccb=1-7&_nc_sid=127cfc&_nc_ohc=aa08-ozdvrkQ7kNvgGr_gbP&_nc_zt=23&_nc_ht=scontent.ftbs5-3.fna&_nc_gid=AQ-CwYmHo_lAPcRq_qVu6We&oh=00_AYDDrgY8JC5CdSrdtts3u8fZbiyK3mFEzb1ebmnXi2ZKqA&oe=67771DC8",
+    category: "გარე მარკეტინგული",
+    photos: [
+      "https://scontent.ftbs5-3.fna.fbcdn.net/v/t39.30808-6/451588293_892421192926552_8467713785957959122_n.jpg?stp=cp6_dst-jpg_tt6&_nc_cat=106&ccb=1-7&_nc_sid=127cfc&_nc_ohc=k_d4sk8FM24Q7kNvgFAW3YR&_nc_zt=23&_nc_ht=scontent.ftbs5-3.fna&_nc_gid=AqnSmJjj5_kKoon-uE0iL9k&oh=00_AYAyS56rpB1flzkL7Bs-_BahaHKdf9Hf07ec3pMJWpT1Iw&oe=6777275A",
+      "https://scontent.ftbs5-3.fna.fbcdn.net/v/t39.30808-6/451872777_892421179593220_1921483017377968548_n.jpg?stp=cp6_dst-jpg_tt6&_nc_cat=109&ccb=1-7&_nc_sid=127cfc&_nc_ohc=cTRDWWc81GgQ7kNvgFLFEfY&_nc_zt=23&_nc_ht=scontent.ftbs5-3.fna&_nc_gid=AbEclkdcJLlmsoo295a2XFc&oh=00_AYD6vs35eHHbq9NawIzd6mogDsu_vUBVbQryZPfFVvfE5Q&oe=67770660",
+    ],
   },
 ];
 
