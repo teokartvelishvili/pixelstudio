cf466fdd951c33228405a031daea364b1fa1e579
anano303 <124179773+anano303@users.noreply.github.com>
2024-12-29 21:43:04 +0400

    contact. add

--- src/Pages/Contact/Contact.jsx
+++ src/Pages/Contact/Contact.jsx
@@ -1,16 +1,66 @@
-import React, { useContext } from "react"
-import { ThemeContext } from "../../Hooks/ThemeContext";
-import { LanguageContext } from "../../Hooks/LanguageContext";
-import { TEXTS } from "../../Hooks/Languages";
-import "./Contact.css";
-
+import React from "react";
+import "./Contact.scss";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faFacebook, faInstagram } from "@fortawesome/free-brands-svg-icons";
 
 const Contact = () => {
-  const { theme } = useContext(ThemeContext);
-  const { language, setLanguage } = useContext(LanguageContext);
-
   return (
-    <div className={`page ${theme}`}>{TEXTS[language].contact}</div>
+    <div className="ContactPage">
+      <hr />
+      <h2> დაგვიკავშირდით </h2>
+      <div className="contact-container">
+        <div className="contact-background">
+          <div className="contact-info">
+            <p>
+              ჩვენი ფოტო სტუდია გთავაზობთ პროფესიონალურ ფოტოგრაფიას და ვიდეო
+              გადაღებას ნებისმიერი მიზნისთვის. თქვენთვის სასურველი შთამბეჭდავი
+              კადრები, რომლებიც აღბეჭდავს თქვენს საუკეთესო მომენტებს.
+            </p>
+            <p>info@pixelstudio.ge</p>
+            <p>123-456-7890</p>
+            <div className="social-icons">
+              <a
+                href="https://facebook.com"
+                className="social-icon"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <FontAwesomeIcon
+                  icon={faFacebook}
+                  style={{ color: "#dfb759", fontSize: "24px" }}
+                />
+              </a>
+              <a
+                href="https://instagram.com"
+                className="social-icon"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <FontAwesomeIcon
+                  icon={faInstagram}
+                  style={{ color: "#dfb759", fontSize: "24px" }}
+                />
+              </a>
+            </div>
+          </div>
+          <div className="contact-form">
+            <form>
+              <div className="form-group">
+                <input type="text" placeholder="სახელი *" required />
+                <input type="text" placeholder="გვარი *" required />
+              </div>
+              <input type="email" placeholder="იმეილი *" required />
+              <input type="text" placeholder="Subject" />
+              <textarea
+                placeholder="დაგვიტოვე შეტყობინება ..."
+                required
+              ></textarea>
+              <button type="submit">გაგზავნა</button>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
   );
 };
 
