16ac62bdede3b41d2299b53c1b086021e7304699
anano303 <124179773+anano303@users.noreply.github.com>
2025-01-09 15:52:59 +0400

    logo

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,48 +1,48 @@
 import React, { useState, useContext } from "react";
-import { ThemeContext } from '../../Hooks/ThemeContext';
+import { ThemeContext } from "../../Hooks/ThemeContext";
 import { LanguageContext } from "../../Hooks/LanguageContext";
 import { Link } from "react-router-dom";
 import { TEXTS } from "../../Hooks/Languages";
 import "./Header.css";
-import Navbar from '../Navbar/Navbar'
-import mainLogo from '../../Assets/PixelStudioLogo.png'
+import Navbar from "../Navbar/Navbar";
+import mainLogo from "../../Assets/Yellow.png";
 import ToggleSwitch from "../Toggle/Toggle";
-import toggleStars from '../../Assets/stars.jpeg'
-import toggleShine from '../../Assets/sunShine.jpeg'
-import geoFlag from '../../Assets/geoFlag.png'
-import engFlag from '../../Assets/engFlag.png'
+import toggleStars from "../../Assets/stars.jpeg";
+import toggleShine from "../../Assets/sunShine.jpeg";
+import geoFlag from "../../Assets/geoFlag.png";
+import engFlag from "../../Assets/engFlag.png";
 
 const Header = () => {
-const { theme, toggleTheme } = useContext(ThemeContext); // Access ThemeContext
-    const [isChecked, setIsChecked] = useState(theme === "dark");
-    const { language, setLanguage } = useContext(LanguageContext);
+  const { theme, toggleTheme } = useContext(ThemeContext); // Access ThemeContext
+  const [isChecked, setIsChecked] = useState(theme === "dark");
+  const { language, setLanguage } = useContext(LanguageContext);
 
-    const handleChange = () => {
-        setIsChecked(!isChecked);
-        toggleTheme(); // Toggle theme
-    };
-    const handleLangClick = () => {
-        const newLanguage = language === "ge" ? "en" : "ge";
-        setLanguage(newLanguage); // Use setLanguage from LanguageContext to update the language
-      };
+  const handleChange = () => {
+    setIsChecked(!isChecked);
+    toggleTheme(); // Toggle theme
+  };
+  const handleLangClick = () => {
+    const newLanguage = language === "ge" ? "en" : "ge";
+    setLanguage(newLanguage); // Use setLanguage from LanguageContext to update the language
+  };
 
-
-return (
+  return (
     <div className={`header ${theme}`}>
-       <Link to="/">
+      <Link to="/">
         <div className="mainLogo">
-            <img src={mainLogo} alt="" />
-        </div></Link>
-        <div className="ThemeToggle">
-            {/* Pass all four images as props */}
-            <div className={`toggles ${theme}`}>
-            {/* <ToggleSwitch 
+          <img src={mainLogo} alt="" />
+        </div>
+      </Link>
+      <div className="ThemeToggle">
+        {/* Pass all four images as props */}
+        <div className={`toggles ${theme}`}>
+          {/* <ToggleSwitch 
                 checked={isChecked} 
                 onChange={handleChange} 
                 image={toggleShine} 
                 image2={toggleStars}
             /> */}
-            {/* <div>
+          {/* <div>
              <img
               className="lang"
               src={language === "ge" ? engFlag : geoFlag}
@@ -50,11 +50,11 @@ return (
               onClick={handleLangClick}
             />
             </div> */}
-            </div></div >
-        <Navbar />
+        </div>
+      </div>
+      <Navbar />
     </div>
-
-);
+  );
 };
 
 export default Header;
--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -6,34 +6,43 @@ import { Link } from "react-router-dom";
 import "./Navbar.css";
 
 const Navbar = () => {
-    const { theme } = useContext(ThemeContext);
-    const { language } = useContext(LanguageContext);
-    const [isOpen, setIsOpen] = useState(false);
+  const { theme } = useContext(ThemeContext);
+  const { language } = useContext(LanguageContext);
+  const [isOpen, setIsOpen] = useState(false);
 
-    const toggleMenu = () => {
-        setIsOpen(!isOpen);
-    };
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
 
-    const closeMenu = () => {
-        setIsOpen(false);
-    };
+  const closeMenu = () => {
+    setIsOpen(false);
+  };
 
-    return (
-        <div className={`NavCont ${theme}`}>
-            <div className="hamburger-menu" onClick={toggleMenu}>
-                <div className={`burger ${isOpen ? 'open' : ''}`}>
-                    <div className="line"></div>
-                    <div className="line"></div>
-                    <div className="line"></div>
-                </div>
-            </div>
-            <ul className={`UlCont ${isOpen ? "open" : ""}`}>
-                <li onClick={closeMenu}><Link to="/">{TEXTS[language].home}</Link></li>
-                <li onClick={closeMenu}><Link to="/About">{TEXTS[language].about}</Link></li>
-                <li onClick={closeMenu}><Link to="/Contact">{TEXTS[language].contact}</Link></li>
-            </ul>
+  return (
+    <div className={`NavCont ${theme}`}>
+      <div className="hamburger-menu" onClick={toggleMenu}>
+        <div className={`burger ${isOpen ? "open" : ""}`}>
+          <div className="line"></div>
+          <div className="line"></div>
+          <div className="line"></div>
         </div>
-    );
+      </div>
+      <ul className={`UlCont ${isOpen ? "open" : ""}`}>
+        <li onClick={closeMenu}>
+          <Link to="/">{TEXTS[language].home}</Link>
+        </li>
+        <li onClick={closeMenu}>
+          <Link to="/About">{TEXTS[language].about}</Link>
+        </li>
+        <li onClick={closeMenu}>
+          <Link to="/About">საქმიანობა</Link>
+        </li>
+        <li onClick={closeMenu}>
+          <Link to="/Contact">{TEXTS[language].contact}</Link>
+        </li>
+      </ul>
+    </div>
+  );
 };
 
 export default Navbar;
