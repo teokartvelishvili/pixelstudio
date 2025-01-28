f2c7c16ee44832f8ff0f47365e94620fa645b95c
anano303 <124179773+anano303@users.noreply.github.com>
2025-01-28 22:11:13 +0400

    სურათების ოპტიმიზაცია

--- src/Components/Carousel/Carousel.jsx
+++ src/Components/Carousel/Carousel.jsx
@@ -4,15 +4,15 @@ import { Link } from "react-router-dom";
 
 const Carousel = () => {
   const images = [
-    "https://res.cloudinary.com/dbc02mn7z/image/upload/v1737913892/IMG_3851_oocsey.jpg",
-    "https://res.cloudinary.com/dbc02mn7z/image/upload/v1737914249/_MG_1175_ab7eda.jpg",
-    "https://res.cloudinary.com/dbc02mn7z/image/upload/v1737913884/Firefly_Inpaint_20230805233255_kaa6ik.png",
-    "https://res.cloudinary.com/dbc02mn7z/image/upload/v1737913881/IMG_4834_ujg6l7.jpg",
-    "https://res.cloudinary.com/dbc02mn7z/image/upload/v1737913872/IMG_2462_t3tmju.jpg",
-    "https://res.cloudinary.com/dbc02mn7z/image/upload/v1737913869/IMG_1366_fdxajh.jpg",
-    "https://res.cloudinary.com/dbc02mn7z/image/upload/v1737913866/IMG_0749_ms8tyi.jpg",
-    "https://res.cloudinary.com/dbc02mn7z/image/upload/v1735510838/IMG_0984_kpglis.jpg",
-    "https://res.cloudinary.com/dbc02mn7z/image/upload/v1737914249/%E1%83%90%E1%83%A1_%E1%83%91%E1%83%94%E1%83%A0%E1%83%92_yxfqo3.jpg",
+    "https://res.cloudinary.com/dbc02mn7z/image/upload/q_auto,f_auto,w_1024,h_768/v1737913892/IMG_3851_oocsey.jpg",
+    "https://res.cloudinary.com/dbc02mn7z/image/upload/q_auto,f_auto,w_1024,h_768/v1737914249/_MG_1175_ab7eda.jpg",
+    "https://res.cloudinary.com/dbc02mn7z/image/upload/q_auto,f_auto,w_1024,h_768/v1737913884/Firefly_Inpaint_20230805233255_kaa6ik.png",
+    "https://res.cloudinary.com/dbc02mn7z/image/upload/q_auto,f_auto,w_1024,h_768/v1737913881/IMG_4834_ujg6l7.jpg",
+    "https://res.cloudinary.com/dbc02mn7z/image/upload/q_auto,f_auto,w_1024,h_768/v1737913872/IMG_2462_t3tmju.jpg",
+    "https://res.cloudinary.com/dbc02mn7z/image/upload/q_auto,f_auto,w_1024,h_768/v1737913869/IMG_1366_fdxajh.jpg",
+    "https://res.cloudinary.com/dbc02mn7z/image/upload/q_auto,f_auto,w_1024,h_768/v1737913866/IMG_0749_ms8tyi.jpg",
+    "https://res.cloudinary.com/dbc02mn7z/image/upload/q_auto,f_auto,w_1024,h_768/v1735510838/IMG_0984_kpglis.jpg",
+    "https://res.cloudinary.com/dbc02mn7z/image/upload/q_auto,f_auto,w_1024,h_768/v1737914249/%E1%83%90%E1%83%A1_%E1%83%91%E1%83%94%E1%83%A0%E1%83%92_yxfqo3.jpg",
   ];
 
   const [currentIndex, setCurrentIndex] = useState(0);
